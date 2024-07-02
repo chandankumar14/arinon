9164a964c453e7c0d361d60ccd42fd6bf14b8faf
Developer <chandankumar75707@gmail.com>
2024-07-02 18:05:21 +0530

    menu bar completed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,6 +10,10 @@ const products = [
   { name: 'Education', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
   { name: 'Finance and banking', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
   { name: 'Internet of things(IOT)', description: 'Get a better understanding', href: '#', icon: ArrowPathIcon },
+  { name: 'Education', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Finance and banking', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+  { name: 'Internet of things(IOT)', description: 'Get a better understanding', href: '#', icon: ArrowPathIcon },
+
 ]
 const callsToAction = [
   { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
@@ -28,40 +32,44 @@ export default function Header() {
   return (
     <main>
       <header className="bg-gray-50 fixed top-0 w-full  shadow-md z-10">
-        <nav className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
+        <nav className="mx-auto flex max-w-7xl items-center p-3 lg:px-8" aria-label="Global">
+
+          <div className="px-10 lg:flex-initial">
             <a href="#" className="-m-1.5 p-1.5">
-             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+              <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
             </a>
           </div>
+
           <div className="flex lg:hidden">
             <button
               type="button"
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(true)}
             >
-             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
+
           <div className="hidden lg:flex lg:gap-x-12">
             <Popover className="relative">
               <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              Categories
+
+                Industries
                 <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
               </PopoverButton>
 
               <PopoverPanel
                 transition
                 className="absolute top-full z-10 mt-4 w-screen  overflow-hidden rounded-xl
-                 bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 
+                 bg-gray-100 shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 
                  data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 
-                 data-[enter]:ease-out data-[leave]:ease-in"
+                 data-[enter]:ease-out data-[leave]:ease-in float-start"
               >
-                <div className="p-4 lg:grid grid-cols-3 float-start">
+                <div className="lg:grid grid-cols-3">
                   {products.map((item) => (
                     <div
                       key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-300"
                     >
                       <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                         <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
@@ -76,12 +84,12 @@ export default function Header() {
                     </div>
                   ))}
                 </div>
-                
+
               </PopoverPanel>
             </Popover>
             <Popover className="relative">
               <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-                Product
+                Services
                 <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
               </PopoverButton>
 
@@ -96,7 +104,7 @@ export default function Header() {
                   {products.map((item) => (
                     <div
                       key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-300"
                     >
                       <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                         <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
@@ -111,74 +119,30 @@ export default function Header() {
                     </div>
                   ))}
                 </div>
-                
-              </PopoverPanel>
-            </Popover>
-            <Popover className="relative">
-              <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-                Product
-                <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-              </PopoverButton>
 
-              <PopoverPanel
-                transition
-                className="absolute top-full z-10 mt-4 w-screen  overflow-hidden rounded-xl
-                 bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 
-                 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 
-                 data-[enter]:ease-out data-[leave]:ease-in"
-              >
-                <div className="p-4 lg:grid grid-cols-3 float-start">
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-                      </div>
-                      <div className="flex-auto">
-                        <a href={item.href} className="block font-semibold text-gray-900">
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </a>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                
               </PopoverPanel>
             </Popover>
 
-              
+
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              WHAT'S NEW
+              Home
             </a>
 
             <a href="" className="text-sm font-semibold leading-6 text-gray-900">
-              SALWAR KAMEEZ
+              About Us
             </a>
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              SAREES
+              Our Values
             </a>
 
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              LEHENGA
-            </a>
-            
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              GOWNS
+              Portfolio
             </a>
 
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              MEN'S WEAR
+              Contact
             </a>
 
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              GOWNS
-            </a>
-
-            
           </div>
 
         </nav>
